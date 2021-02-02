68c7724c21e674efe1fff82c601f241d1aebe282
gugubird404 <862381272@qq.com>
2021-02-03 01:04:29 +0800

    页面提供

---
+++ trend-trading-backtest-view8041/src/main/java/gugu/trend/Main8041.java
@@ -0,0 +1,17 @@
+package gugu.trend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 页面跳转
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Main8041 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8041.class, args);
+    }
+}
