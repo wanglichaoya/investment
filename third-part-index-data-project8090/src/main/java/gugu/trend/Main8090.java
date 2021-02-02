384fb6e50f11be8ac8ae2f24a7658de3cd2f173b
gugubird404 <862381272@qq.com>
2021-02-03 01:00:31 +0800

    模拟第三方提供的数据服务api

---
+++ third-part-index-data-project8090/src/main/java/gugu/trend/Main8090.java
@@ -0,0 +1,15 @@
+package gugu.trend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 模拟的第三方数据服务
+ */
+@SpringBootApplication
+public class Main8090 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8090.class, args);
+    }
+}
+
