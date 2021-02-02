36e298d50d53e2d25f2fe4c3bd0609f3b0562817
gugubird404 <862381272@qq.com>
2021-02-03 00:58:56 +0800

    网关服务

---
+++ index-getway-server8888/src/main/java/gugu/trend/Main8888.java
@@ -0,0 +1,13 @@
+package gugu.trend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Main8888 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8888.class,args);
+    }
+}
