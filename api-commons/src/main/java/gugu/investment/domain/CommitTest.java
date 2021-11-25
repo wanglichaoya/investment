a04917a8b57d97b9cd98674afb82a2f9f75abd3d
longguotao <longgt9898@163.com>
2021-11-25 17:37:31 +0800

    test: test
    - 测试 NullPointerException

--- api-commons/src/main/java/gugu/investment/domain/CommitTest.java
+++ api-commons/src/main/java/gugu/investment/domain/CommitTest.java
@@ -2,10 +2,13 @@ package gugu.investment.domain;
 
 public class CommitTest {
     public static void main(String[] args) {
-
+        Integer i = null;
+        int a = 1;
+        method1(i);
     }
 
-    private void  method1(){
-        
+    private static void  method1(int i){
+        i+=1;
+        System.out.println(1);
     }
 }
